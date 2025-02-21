b6de2a48bfda10bb2ae0c8dfd30709332d7b671b
rclark <rclark0003@gmail.com>
2025-02-21 02:47:19 +0000

    chaning the app file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
+st.set_page_config(
 
-st.title("🎈 My new Streamlit app")
+    page_title='Hello world',
+    layout='centered',
+    initial_sidebar_state='auto',
+    menu_items={
+        'Get Help': 'https://streamlit.io/',
+        'Report a bug': 'https://github.com',
+        'About': 'About your application: **Hello world**'
+        }
+)
+st.sidebar.title('🎈Hello world')
+st.title('🎈Hello world')
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "This is my Hello World app"
 )
