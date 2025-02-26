5c10d997c76f0ad9020fc72cb442a884c196181c
rclark <rclark0003@gmail.com>
2025-02-26 02:20:10 +0000

    copying

---
+++ streamlit_app_copy.py
@@ -0,0 +1,17 @@
+import streamlit as st
+st.set_page_config(
+
+    page_title='Hello world',
+    layout='centered',
+    initial_sidebar_state='auto',
+    menu_items={
+        'Get Help': 'https://streamlit.io/',
+        'Report a bug': 'https://github.com',
+        'About': 'About your application: **Hello world**'
+        }
+)
+st.sidebar.title('🎈Hello world')
+st.title('🎈Hello world')
+st.write(
+    "This is my Hello World app"
+)
